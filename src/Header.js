2f9e3bce7e417e1cb36570e560008db05335f162
3nsar <84131267+3nsar@users.noreply.github.com>
2022-09-17 08:30:18 +0200

    header and main

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+
+export const Header = ()=>{
+    return(
+    <div className="nav-container">
+        <div className="nav-content">
+            <h1>Random-Facts</h1>
+            <p>learn something new</p>
+        </div>
+    </div>
+    )
+}
---
+++ src/MainContent.js
@@ -0,0 +1,19 @@
+
+export const MainContent = ()=>{
+    return(
+    <div className="main-container">
+        <div className="main-content">
+            <ul>
+                <li>There are 31,556,926 seconds in a year</li>
+                <li>A crocodile cannot stick its tongue out</li>
+                <li>The Philippines consists of 7,641 islands</li>
+                <li>A shrimp's heart is in its head</li>
+                <li>Lemons float, but limes sink</li>
+                <li>A shark is the only known fish that can blink with both eyes</li>
+                <li>Scotland has 421 words for snow</li>
+                <li>Armadillo shells are bulletproof</li>
+                <li>Thanks to 3D printing, NASA can basically email tools to astronauts</li>
+            </ul>
+        </div>
+    </div>)
+}
